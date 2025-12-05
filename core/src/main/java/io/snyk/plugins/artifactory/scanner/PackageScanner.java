46f0b24d76b2ab3513e549b78907f916a05e1976
Alex Rosenzweig <arosenzweig@squareup.com>
2025-12-05 17:59:21 +1100

    fix: more stuff

--- core/src/main/java/io/snyk/plugins/artifactory/scanner/PackageScanner.java
+++ core/src/main/java/io/snyk/plugins/artifactory/scanner/PackageScanner.java
@@ -4,6 +4,10 @@ import io.snyk.plugins.artifactory.model.TestResult;
 import org.artifactory.fs.FileLayoutInfo;
 import org.artifactory.repo.RepoPath;
 
+import java.time.Instant;
+
 public interface PackageScanner {
   TestResult scan(FileLayoutInfo fileLayoutInfo, RepoPath repoPath);
+
+  //Instant getPackageReleaseDate(String packageName, String packageVersion);
 }
